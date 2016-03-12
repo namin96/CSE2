4fdc4e33aa6b1b5904413f4f1a2bdeb3bfd4ce7a
namin96 <nea318@lehigh.edu>
2016-03-12 15:04:25 +0000

    updated Twist Generator

--- lab06/TwistGenerator.java
+++ lab06/TwistGenerator.java
@@ -8,27 +8,84 @@ which consists of slashes and X's. The entered integer will serve as the twist l
 */
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class TwistGenerator{
     public static void main(String[] args){
         
         Scanner input = new Scanner(System.in);
-        
-        
         System.out.print("Enter a positive integer value: ");
-        int num = input.nextInt();
+        String line = input.nextLine();
+        
+        while(!line.equals("-1")) {
+			try {
+				int num = Integer.parseInt(line);
+				System.out.println("Length: " + num);
+				
         
         while(num < 1){
             System.out.print("Please enter a postive integer value: ");
             num = input.nextInt();
         }
         
-        int count = 0;
+        int count = 1;
         
-        while(count < num){
-            System.out.println("\\");
+        while(count <= num){
+            if(count % 3 == 1){
+                System.out.print("\\");
+            }
+            else if (count % 3 == 2){
+                System.out.print(" ");
+            }
+            else if (count % 3 == 0){
+                System.out.print("/");
+            }
+            
+            if(count == num){
+                System.out.println();
+            }
+            count ++;
+        }
+        count = 1;
+        while (count <= num){
+            if (count % 3 == 1){
+                System.out.print(" ");
+            }
+            else if(count % 3 == 2){
+                System.out.print("X");
+            }
+            else if(count % 3 == 0){
+                System.out.print(" ");
+            }
+            if (count == num){
+                System.out.println();
+            }
             count ++;
             
         }
+        count = 1;
+        while (count <= num){
+            if(count % 3 == 1){
+                System.out.print("/");
+            }
+            else if(count % 3 == 2){
+                System.out.print(" ");
+            }
+            else if(count % 3 == 0){
+                System.out.print("\\");
+            }
+            if (count == num){
+                System.out.println();
+            }
+            count ++;
+			} 
+			}catch(NumberFormatException e) {
+				System.out.println("Please enter a positive integer only: ");
+			}
+			
+			line = input.nextLine();
+		}
+        
     }
 }
+
